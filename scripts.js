e1dd964bde25491eb244fb4e3b60284b6f409e24
João Pedro Oliveira <88394758+JoaoP-Oliver@users.noreply.github.com>
2022-03-07 17:47:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,10 @@
-        function notes() {
-            let request = document.getElementByd('tarefa')
-            let response = document.getElementById('list')
-        }
+function tsx() {
+    let request = document.getElementById('tarefa').value;
+    const div = document.createElement('div');
+    div.setAttribute('class', 'res')
+    div.innerHTML = `${request}`;
+    
 
-
-        function error() {
-            const div = document.createElement('div')
-            div.innerHTML = `
-            <div id="list">
-            <p>No dia mais claro na noite mais densa o mal sucumbirá ante a minha presença, E todo aquele que venera o mal há de penar quando o poder do lanterna verde enfrentar!</p>
-            </div>
-            `
-
-            const body = document.querySelector('body')
-            body.append(div)
-
-        }
-
-        error()
-        error()
-        error()
-        error()
-        error()
+    const body = document.querySelector('body');
+    body.append(div)
+}
