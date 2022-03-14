24ccc3f9e39a2eff8445535a041579376174203e
João Pedro Oliveira <88394758+JoaoP-Oliver@users.noreply.github.com>
2022-03-13 21:52:58 -0300

    Update scripts.js
    Adicionada a função de impedir o usuario de adicionar uma tarefa sem ter escrito nada.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
-function tsx() {
-    let request = document.getElementById('tarefa').value;
-    const div = document.createElement('div');
-    div.setAttribute('class', 'res')
-    div.innerHTML = `${request}`;
-    
+      function tsx() {
+        let request = document.getElementById('tarefa').value;
 
-    const body = document.querySelector('body');
-    body.append(div)
-}
+        const div = document.createElement('div');
+        div.setAttribute('class', 'res')
+        div.innerHTML = `
+        ${request}
+
+        <button id="trash" onclick="removeDiv()"> Apagar </button>
+        `;
+        
+        if(request === "") {
+          window.alert('Digite alguma coisa')
+
+          return
+        }
+
+        const body = document.querySelector('body');
+        body.append(div)
+      }
+
+      function removeDiv() {
+        const res = document.querySelector('res')
+
+        res.remove()
+      }
